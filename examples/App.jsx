e193e86e28029d6260d4e021aadbf5bb3dc7b131
Bernardo Raposo <braposo@gmail.com>
2018-12-24 11:37:40 +0000

    Adds noWrap prop
    By default whitespace was set to nowrap so now we want to only enable that as a prop

--- examples/App.jsx
+++ examples/App.jsx
@@ -5,22 +5,40 @@ import cxs from "cxs";
 class App extends React.PureComponent {
     render() {
         const styles = cxs({
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "90vh",
+            display: "block",
         });
 
         return (
-            <div className={styles}>
-                <h1 style={{ width: 500, textAlign: "left" }}>
-                    Cool sentence,{" "}
-                    <TextLoop interval={3000}>
-                        <span speed={200}>a</span>
-                        <span>b</span>
-                        c
-                    </TextLoop>
-                </h1>
+            <div>
+                <div className={styles}>
+                    <h2 style={{ width: 500, textAlign: "left" }}>
+                        Cool sentence,{" "}
+                        <TextLoop interval={3000}>
+                            <span speed={200}>one</span>
+                            <span>two</span>
+                            <span>three</span>
+                        </TextLoop>
+                    </h2>
+                </div>
+                <div className={styles}>
+                    <h2 style={{ width: 500, textAlign: "center" }}>
+                        Cool sentence,{" "}
+                        <TextLoop interval={3000}>
+                            <span speed={200}>one</span>
+                            <span>two</span>
+                            <span>three</span>
+                        </TextLoop>
+                    </h2>
+                </div>
+                <div className={styles}>
+                    <h2 style={{ width: 200 }}>
+                        <TextLoop interval={3000}>
+                            <span>Cool sentence, one</span>
+                            <span>Cool sentence, two</span>
+                            <span>Cool sentence, three</span>
+                        </TextLoop>
+                    </h2>
+                </div>
             </div>
         );
     }
